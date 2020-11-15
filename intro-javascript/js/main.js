c91e773e143bdc69e2ab08ffae554ffb9e164e80
Pedro <phenriqueleao@gmail.com>
2020-11-15 14:45:26 -0300

    Netflix Clone

---
+++ intro-javascript/js/main.js
@@ -0,0 +1,96 @@
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+
+    //console.log(document.getElementById("agradecimento"));
+    //alert('Obrigado por clicar');
+}
+
+function redirecionar() {
+    window.open("https://globallabs.academy/");
+    window.location.href = "https://globallabs.academy/";
+}
+
+function trocar(elemento) {
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+
+// function validaIdade(idade) {
+//     var validar;
+//     if (idade >= 18) {
+//         validar = true
+//     } else {
+//         validar = false
+//     }
+//     return validar;
+// }
+
+// var idade = prompt("Qual sua idade?");
+// console.log(validaIdade(idade));
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+/*
+var count;
+for(count = 0; count <= 5; count++) {
+    alert(count);
+}
+*/
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+}
+*/
+/*
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+// console.log(lista);
+// console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+// var nome = "Pedro Leão";
+// var n1 = 5;
+// var n2 = 3;
+// var frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " + idade + " anos.");
+// alert(idade + idade2);
+// console.log(nome);
+// console.log(n1 * n2);
+// console.log(frase.toLowerCase());
+// alert(frase.replace("Japão", "Brasil"));
